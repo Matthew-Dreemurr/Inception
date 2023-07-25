40f7ec8790f0000e342a87944be95f0d4fb93bbb
matthew-dreemurr <hadadmat@gmail.com>
2023-07-25 15:56:29 +0000

    [WIP] wp install script

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-echo "Staring php-fpm"
+echo "[INFO] Create wordpress config"
 
-mkdir -p /usr/logs/php-fpm
+echo "[INFO] connecting to db"
+wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOST
+
+echo "[INFO] Check db connection"
+wp db check
+
+if [ $? -eq 0 ]; then
+	echo "[INFO] db connected"
+else
+	echo "[ERROR] db connection faild!"
+	exit 1;
+fi
+
+# echo "[INFO] Create admin user"
+
+echo "[INFO] Starting php-fpm"
+
 php-fpm -F
